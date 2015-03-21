8b6d567a3467fe42cc1b23e2ce129c1e65cad3ab
Anastasios Zouzias <zouzias@gmail.com>
2015-03-21 11:48:18 +0100

    Destroy update

--- actions/destroyAllDroplets.py
+++ actions/destroyAllDroplets.py
@@ -1,6 +1,13 @@
-import digitalocean
-manager = digitalocean.Manager(token="XXXX")
+#!/usr/local/bin/python
 
-my_droplets = manager.get_all_droplets()
-for droplet in my_droplets:
-    droplet.destroy()
+import digitalocean
+
+with open ("token-api.txt", "r") as file:
+    token = file.read().replace('\n', '')
+    print "Using API token: " + token
+    manager = digitalocean.Manager(token=token)
+    my_droplets = manager.get_all_droplets()
+    print str(len(my_droplets)) + " droplets found..."
+    for droplet in my_droplets:
+        print "Destroying droplet" + str(droplet)
+        droplet.destroy()
