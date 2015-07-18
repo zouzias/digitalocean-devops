0198269ab5657226d95f1f2592581bc9c485fea2
Anastasios Zouzias <zouzias@gmail.com>
2015-07-19 00:37:29 +0200

    Remove actions

--- actions/shutdownAllDroplets.py
+++
@@ -1,8 +0,0 @@
-import digitalocean
-
-with open ("token-api.txt", "r") as file:
-    token = file.read()
-    manager = digitalocean.Manager(token=token)
-    my_droplets = manager.get_all_droplets()
-    for droplet in my_droplets:
-        droplet.shutdown()
